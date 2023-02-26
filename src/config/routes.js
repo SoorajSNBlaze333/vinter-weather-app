1f137169426b0554cf5816f838e057c4429594b5
SoorajSNBlaze333 <soorajsnblaze333@gmail.com>
2023-02-26 14:27:16 -0600

    Updated CORS blockers

--- src/config/routes.js
+++ src/config/routes.js
@@ -4,6 +4,6 @@ export const METEOMATICS_WEATHER = "https://api.meteomatics.com/";
 export const DEVELOPEMENT_SERVER = "http://localhost:3000/api";
 export const PRODUCTION_SERVER = "https://main.d1euh075gobjok.amplifyapp.com/api";
 
-const BASE_URL = DEVELOPEMENT_SERVER
+const BASE_URL = PRODUCTION_SERVER
 export const LOGIN_URL = BASE_URL + "/login";
 export const WEATHER_URL = (parameters) => BASE_URL + `/weather?${parameters}`;
